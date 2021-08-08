7b2463682e6d60319e3a41020c7a961f1d5b2d83
naotovader <gallardo.st62@gmail.com>
2021-08-08 13:08:02 +0900

    aaa

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,11 +4,8 @@ import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
 
-<<<<<<< HEAD
-ß
-=======
+aaa
 
->>>>>>> 20488e2257e16a17e7c5b459ec5acdad14eccebf
 export default function Post({ postData }) {
   return (
     <Layout>
