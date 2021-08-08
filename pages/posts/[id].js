1352d5ae7fbed08c4f6502e324c884f11af35ae5
naotovader <gallardo.st62@gmail.com>
2021-08-08 13:14:37 +0900

    delete space

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,8 +4,6 @@ import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
 
-aaa
-
 export default function Post({ postData }) {
   return (
     <Layout>
